2a822ad704a2568e26438e866ff8a519772c15e8
iyaremye-faustin <iyaremyef@gmail.com>
2022-05-10 16:57:15 +0200

    ch-setup-sass-processing-181414556

---
+++ src/components/demo/DemoSassComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/DemoSassCss.css';
+
+export default function DemoSassComponent() {
+  return (
+    <div className="nav-demo">
+      <p>Styled By The Processed Sass Css styles</p>
+    </div>
+  );
+}
--- src/redux/features/dummy.feature.js
+++ src/redux/features/dummy.feature.js
@@ -1,19 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState =  {color:""};
+const initialState = { color: '' };
 
 export const dummySlice = createSlice({
-    name: "dummy",
-    initialState: initialState,
-    reducers: {
-        changeColor: (state, action) => {
-            state.color = action.payload;
-        },
-        resetColor: (state) => {
-            state.color = "black";
-        },
+  name: 'dummy',
+  initialState,
+  reducers: {
+    changeColor: (state, action) => {
+      state.color = action.payload;
     },
-})
+    resetColor: (state) => {
+      state.color = 'black';
+    },
+  },
+});
 
-export const {changeColor,resetColor} = dummySlice.actions;
+export const { changeColor, resetColor } = dummySlice.actions;
 export default dummySlice.reducer;
